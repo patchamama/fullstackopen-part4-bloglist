7c913309759236084783f4bdfdec443c5b9e19d5
A. Urquiola <patchamama.go@gmail.com>
2023-09-09 15:29:31 +0200

    add midlewares

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const BlogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -19,8 +20,13 @@ mongoose
   })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', BlogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
