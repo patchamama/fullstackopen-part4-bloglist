f1433ccf1e344a61f060d3c7c8a0e4c939012174
A. Urquiola <patchamama.go@gmail.com>
2023-09-09 15:17:38 +0200

    refactoring: add controllers/blogs

--- apps.js
+++ apps.js
@@ -2,9 +2,8 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const BlogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
-
-const Blog = require('../models/blog')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -16,16 +15,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+app.use('/api/notes', BlogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+module.exports = app
