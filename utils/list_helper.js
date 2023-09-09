c9d554cf41d5cbe5992fd2e2005564fab816d8ad
A. Urquiola <patchamama.go@gmail.com>
2023-09-09 17:46:00 +0200

    solution 4.6*: funciones auxiliares y pruebas unitarias, paso 4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   // ...
   return 1
@@ -14,12 +16,27 @@ const favoriteBlog = (blogs) => {
   const reducer = (max, item) => {
     return max.likes > item.likes ? max : item
   }
-  console.log(blogs.reduce(reducer, 0))
+  //   console.log(blogs.reduce(reducer, 0))
   return blogs.length === 0 ? {} : blogs.reduce(reducer, { likes: 0 })
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return { author: undefined, entries: 1 }
+  else if (blogs.length === 1) return { author: blogs[0].author, entries: 1 }
+  else {
+    const blogsByAuthor = _.groupBy(blogs, 'author')
+    const authorEntries = _.map(blogsByAuthor, (blogs, author) => ({
+      author,
+      entries: blogs.length,
+    }))
+    console.log(_.maxBy(authorEntries, 'entries'))
+    return _.maxBy(authorEntries, 'entries')
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
